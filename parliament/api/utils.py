e60f4a5f00dc8bc55d8baf8ac490e1149ced8551
vdragan <vdragan1993@gmail.com>
2016-06-13 19:35:28 +0200

    Implementirana validacija na osnovu XML seme.

--- parliament/api/utils.py
+++ parliament/api/utils.py
@@ -1,2 +1,39 @@
-__author__ = 'Dragan Vidakovic'
-# other help methods
+from lxml import etree, objectify
+from lxml.etree import XMLSyntaxError
+
+
+# location of xsd
+xsd_act = 'schema/XML_Proj_schema_act.xsd'
+xsd_amendment = 'schema/XML_Proj_schema_amandman.xsd'
+
+
+def validate_act(act_xml_string):
+    """
+    Validates act with an XML schema
+    :param act_xml_string: XML string of an act, UTF-8 encoded
+    :return: True or False
+    """
+    try:
+        schema = etree.XMLSchema(file=xsd_act)
+        parser = objectify.makeparser(schema=schema)
+        objectify.fromstring(act_xml_string, parser)
+        return True
+    except XMLSyntaxError:
+        return False
+        pass
+
+
+def validate_amendment(amendment_xml_string):
+    """
+    Validates amendment with an XML schema
+    :param amendment_xml_string: XML string of an amendment, UTF-8 encoded
+    :return: True or False
+    """
+    try:
+        schema = etree.XMLSchema(file=xsd_amendment)
+        parser = objectify.makeparser(schema=schema)
+        objectify.fromstring(amendment_xml_string, parser)
+        return True
+    except XMLSyntaxError:
+        return False
+        pass
